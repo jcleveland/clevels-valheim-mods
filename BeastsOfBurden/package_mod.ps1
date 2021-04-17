db797df9759c4f087a4a09265f49031920411493
clevel <jkclevel@gmail.com>
2021-04-17 16:57:32 -0400

    Minor changes to package script.

--- BeastsOfBurden/package_mod.ps1
+++ BeastsOfBurden/package_mod.ps1
@@ -1,12 +1,14 @@
 # Mod DLL
-$mod_dll = ".\bin\Release\BeastsOfBurden.dll"
 $mod_name = "BeastsOfBurden"
+$project_dir = ".\"
+$output_dir = $project_dir + "\bin\Release\"
+$mod_dll = $output_dir + $mod_name + ".dll"
 
 # Get mod version from assembly version info
 $mod_version = (Get-Command $mod_dll).FileVersionInfo.FileVersion
 
 # Locations to put artifacts
-$artifact_path = ".\build-artifacts\"
+$artifact_path = $output_dir + "artifacts\"
 $nexus_path = $artifact_path + "nexus\"
 $tsio_path = $artifact_path + "ts_io\"
 $raw_path = $artifact_path + "raw\" + $mod_name + $mod_version
@@ -31,17 +33,17 @@ Compress-Archive -Path $mod_dll -DestinationPath $nexus_zip -Force
 ####### Thunderstore packaging
 ###################################
 # Create temp directory for TSIO package
-$tsio_tmp_directory = $env:TEMP + "\ts_io_tmp\" + $mod_name + $mod_version + "\"
+$tsio_tmp_directory = $output_dir + "ts_io_tmp\" + $mod_name + $mod_version + "\"
 New-Item -ItemType Directory -Force -Path $tsio_tmp_directory
 
 # Update Manifest to have correct version number
-$manifest = Get-Content ".\resources\manifest.json" -raw | ConvertFrom-Json
+$manifest = Get-Content $project_dir"resources\manifest.json" -raw | ConvertFrom-Json
 $manifest.version_number = $mod_version
 $manifest | ConvertTo-Json -depth 32| set-content $tsio_tmp_directory"manifest.json"
 
 # Copy README and icon into tmp directory
-Copy-Item "README.md" $tsio_tmp_directory
-Copy-Item ".\resources\icon.png" $tsio_tmp_directory
+Copy-Item $project_dir"README.md" $tsio_tmp_directory
+Copy-Item $project_dir"resources\icon.png" $tsio_tmp_directory
 
 # Copy mod dll into tmp file\plugins directory
 New-Item -ItemType Directory -Path $tsio_tmp_directory"files\plugins\" -Force
